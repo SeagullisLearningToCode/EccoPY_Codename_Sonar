01b6193ccf8ade118318c5705a4f1c3a626fd7d2
SIS <scott.colten12@gmail.com>
2021-08-05 20:14:45 -0400

    New File

---
+++ data/mu/Values.py
@@ -0,0 +1,14 @@
+# init/
+#   vars/
+#       str/
+#           video/
+#               res/
+in_between_entries = "x"
+#       int/
+#           video/
+#               wri/
+weather_intensity_scale_limits = [0.0, 10.0]
+#       boolean/
+#           video/
+#               dir/
+r_disable_ripple = False

c64ffa861f661329fa4dc078209339a54f3878f8
SIS <scott.colten12@gmail.com>
2021-08-16 19:48:07 -0400

    Added 2 functions to the `pygame_Tk_Integration` class called "`FinishDict()`" and "`CombineDictToOne()`" as well as a new var called "`self.pg_keyb_list`"

--- data/frw/GF/EXT/GFTKE.py
+++ data/frw/GF/EXT/GFTKE.py
@@ -11,6 +11,7 @@ This file is also makes stating certain things faster and possibly easier.
 # IMPORTS---------------------------------------------------------------------------------------------------------------------
 from tkinter import *
 from tkinter import ttk
+from curses import *
 
 import pygame.event
 from PIL import ImageTk, Image
@@ -109,19 +110,84 @@ class pygame_Tk_Integration(object):
         #       pg/
         #           keyb/
         self.keyi = KEYDOWN
+        #       dict/
+        self.pg_keyb_list = {
+            "alphabet": {
+                "no-mod": ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+                           "a", "s", "d", "f", "g", "h", "j", "k", "l",
+                           "z", "x", "c", "v", "b", "n", "m"],
+                "mod": [] # ;Gull, copy the values from self.pg_keyb_list["alphabet"]["no-mod"] and paste into self.pg_keyb_list["alphabet"]["mod"] as uppercase values
+            },
+            "numerical": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+            "symbolics": {
+                "no-mod": ["`", "-", "=", "[", "]", "\\", ";", "'", ",", ".", "/"],
+                "mod": ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "|", ":", '"', "<", ">", "?"],
+            },
+            "tc": ["backspace", "tab", "pause", "delete", "end", "page up", "page down", "home", "insert"], # ;Text Commands
+            "arrow_keys": {
+                "3bttn": ["up", "down", "left", "right"],
+                "4bttn": [],
+                "9bttn": []
+            }
+        }
 
-    def GetAllKeyBinds(self):
-        # init/
-        #   var/
-        #       arr/
-        pressed_values = []
-        # code/
-        for event in pygame.event.get():
-            if event.type == self.keyi:
-                for key_ in key.get_pressed():
-                    if key_ == 1:
-                        pressed_values.append(key_)
-                        p(f"Table: {pressed_values}\nNumber of pressed keys: {len(pressed_values)}\nTrue Amount: {self.amount_pressed}\nKey Pressed (last key): {key.name(key_)}")
-                        self.amount_pressed += 1
-                p(key.name(self.keyi))
+    def FinishDict(self):
+        """
+        This function shouldn't be runned
+
+        :return:
+        """
+        # INIT/
+        #   VAR/
+        #       BOOLEANS/
+        SHOWRESULT = False
+        # CODE/
+        for key in self.pg_keyb_list["alphabet"]["no-mod"]:
+            self.pg_keyb_list["alphabet"]["mod"].append(key.upper())
+        if SHOWRESULT is True:
+            p(self.pg_keyb_list["alphabet"]["mod"])
+
+    def CombineDictToOne(self, **kwargs):
+        """
+        :param kwargs:
+        :return: allvars
+        """
+        # INIT/
+        #   VAR/
+        #       ARR/
+        allvars = []
+        #       KWARGS/
+        #           BOOLEANS/
+        IncludeAlphaCaps = kwargs.get("include_alpha_caps", False)
+        # CODE/
+        # ;Alpha
+        self.FinishDict()
+        for key_alpha_nm_val in self.pg_keyb_list["alphabet"]["no-mod"]:
+            allvars.append(key_alpha_nm_val)
+
+        if IncludeAlphaCaps is True:
+            for key_alpha_m_val in self.pg_keyb_list["alphabet"]["mod"]:
+                allvars.append(key_alpha_m_val)
+
+        # ;Numbers
+        for key_num_val in self.pg_keyb_list["numerical"]:
+            allvars.append(key_num_val)
+
+        # ;Symbolic
+        for key_sym_nm_val in self.pg_keyb_list["symbolics"]["no-mod"]:
+            allvars.append(key_sym_nm_val)
+
+        for key_sym_m_val in self.pg_keyb_list["symbolics"]["mod"]:
+            allvars.append(key_sym_m_val)
+
+        # ;tc
+        for key_tc_val in self.pg_keyb_list["tc"]:
+            allvars.append(key_tc_val)
+
+        # ;ArrowKeys
+        for key_aw_val in self.pg_keyb_list["arrow_keys"]["3bttn"]:
+            allvars.append(key_aw_val)
+
+        return allvars
+
 # EOF----------------------------------------------------------------------------------------------------------------------------------------------------------------------

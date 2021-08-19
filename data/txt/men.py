fdb1b4efcf5877df4c9c1a307c81a969a2053320
SIS <scott.colten12@gmail.com>
2021-08-19 19:21:53 -0400

    Adjusted the path for `winstrings['main']['options']['Game']`

--- data/txt/men.py
+++ data/txt/men.py
@@ -1,7 +1,6 @@
 # txt/
 #   imp/
 from data.frw.GF.GF import td_c_s_yo, flp
-
 #   vars/
 #       str/
 #           eng/
@@ -14,10 +13,10 @@ winstrings = {
         "options": {
             "Video": {
                 "tooltips": [
-                    "EccoPY uses an external and internal resolution system\n Now with ""Window Resolution"", this will change the actual size of the window without affecting the internal resolution.",  # Window Resolution
-                    "EccoPY uses an external and internal resolution system\n Now with ""2D Resolution"" this will change the game's resolution without resizing the window.",  # 2D Resolution
-                    "In the original Ecco, there a ripple effect that makes the water surface feels alive, disabling this will make the game run slightly faster but will remove the feel of the original game.",  # Disable Ripple
-                    "This is a new addition to Ecco the Dolphin, this adds weather effects based on the level that Ecco's in, this includes rain, snow, fog and possibly enviromental effects"  # Weather Effects Intensity
+                    "EccoPY uses an external and internal resolution system\n Now with ""Window Resolution"", this will change the actual size of the window without affecting the internal resolution.",  # ;Window Resolution
+                    "EccoPY uses an external and internal resolution system\n Now with ""2D Resolution"" this will change the game's resolution without resizing the window.",  # ;2D Resolution
+                    "In the original Ecco, there a ripple effect that makes the water surface feels alive, disabling this will make the game run slightly faster but will remove the feel of the original game.",  # ;Disable Ripple
+                    "This is a new addition to Ecco the Dolphin, this adds weather effects based on the level that Ecco's in, this includes rain, snow, fog and possibly enviromental effects"  # ;Weather Effects Intensity
                 ],
                 "set": ["Window Resolution (size)", "2D Resolution", "Disable Ripple", "Weather Effects intensity"]
             },
@@ -46,31 +45,32 @@ winstrings = {
             "Controlls": {
                 "tooltips": ["Bind Controls"],
                 "set": [
-                    ["Enable Debug Bindings"], # ;Setting Strings
-                    ["Keyboard", "Controller", "Joystick"] # ; User Input type (note that joystick is the controller alt name)
+                    ["Enable Debug Bindings"],  # ;Setting Strings
+                    ["Keyboard", "Controller", "Joystick"]  # ;User Input type (note that joystick is the controller alt name)
                 ],
                 "bindings": [
-                            ["Up", "Down", "Left", "Right"],  # Movement
-                            ["Swim", "Sonar", "Dash", "Pause", "Exit game"],  # Action Buttons
-                            ["TurnOffMusic", "TurnOffSfx"],  # Pause Buttons
-                            ["ShowFPS", "ShowPOS", "ShowAll"],  # Debug
-                        ],
-                "bindings_names": ["movement".title(), "action buttons".title(), "pause buttons".title(), "debug buttons".title()]
-            }
-        },
-        "Game": {
-            "tooltips": [],
-            "set": {
-                "Difficulty": [
-                    ["Truely Alone Mode", "Exploration Mode"],  # Passive modes
-                    ["Easy", "Normal", "Hard", "Extreme"],  # Normal difficulty settings
-                    ["Agressive", "No mercy", "Survival", "One Life mode"]  # Extreme Modes
+                    ["Up", "Down", "Left", "Right"],  # ;Movement
+                    ["Swim", "Sonar", "Dash", "Pause", "Exit game"],  # ;Action Buttons
+                    ["TurnOffMusic", "TurnOffSfx"],  # ;Pause Buttons
+                    ["ShowFPS", "ShowPOS", "ShowAll"],  # ;Debug
                 ],
-                "Game": [
-                    ["Ecco the Dolphin", "Ecco: The Tides of Time"],  # Retail Releases
-                    [],  # Mod Names go here
-                    ["Playable Review", "0429", "X11"]  # Prototypes any pre-release build goes here
-                ]
+                "bindings_names": ["movement".title(), "action buttons".title(), "pause buttons".title(), "debug buttons".title()]
+            },
+            "Game": {
+                "tooltips": [],
+                "set": {
+                    "Difficulty": [
+                        ["Truely Alone Mode", "Exploration Mode"],  # ;Passive modes
+                        ["Easy", "Normal", "Hard", "Extreme"],  # ;Normal difficulty settings
+                        ["Agressive", "No mercy", "Survival", "One Life mode"],  # ;Extreme Modes
+                        ["Randomized (Each enemy)", "Randomized (Truely)"] # ;Misc
+                    ],
+                    "Game": [
+                        ["Ecco the Dolphin", "Ecco: The Tides of Time"],  # ;Retail Releases
+                        [],  # ;Mod Names go here
+                        ["Playable Review", "0429", "X11"]  # ;Prototypes any pre-release build goes here
+                    ]
+                }
             }
         },
         "save_options": ["Ok", "Apply", "Cancel"],
